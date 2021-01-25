44646e997b813e551e400b317ce4d7ac6fadee75
HP <HP@DESKTOP-MB3IDCM>
2021-01-25 23:12:55 +0530

    25/01/2021

---
+++ DataStructure/src/Sorting/BinarySort.java
@@ -0,0 +1,40 @@
+package Sorting;
+import java.util.*;
+public class BinarySort 
+{
+	public static void main(String[] ar)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter the number of element:");
+		int n=sc.nextInt();
+		int element[]=new int[n];
+		System.out.print("Enter array elements:");
+		for(int i=0;i<n;i++)
+		{
+			element[i]=sc.nextInt();
+		}
+		int start=0,end=n-1;
+		while(start<end)
+		{
+			if(element[start]==0)
+			{
+				int temp=element[start];
+				element[start]=element[end];
+				element[end]=temp;
+				if(element[start]==1)
+				{
+					start++;
+				}
+				end--;
+			}
+			else
+			{
+				start++;
+			}
+		}
+		for(int i=0;i<n;i++)
+		{
+			System.out.print(element[i]+" ");
+		}
+	}
+}
